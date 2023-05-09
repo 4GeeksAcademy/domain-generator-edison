6c6cb14ed72f75d44a420b0b4dfe4880836c111a
edijavier99 <126507385+edijavier99@users.noreply.github.com>
2023-05-09 11:29:13 +0000

    ramdon domain generator

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +6,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("myDomain").innerHTML = ramdonDomain();
+};
+
+let ramdonDomain = () => {
+  let pronoun = ["the", "our", "his", "your"];
+  let adj = ["great", "big", "small", "dark"];
+  let noun = ["jogger", "racoon", "three", "car"];
+  let extension = [".com", ".net", ".es", ".io"];
+
+  return (
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.length)] +
+    noun[Math.floor(Math.random() * noun.length)] +
+    extension[Math.floor(Math.random() * extension.length)]
+  );
 };
